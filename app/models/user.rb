1ed624d3b9ec2938c91a7ca48539df8ab0c38f98
mohi <mohisadeghi302@gmail.com>
2023-05-06 13:13:00 +0430

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates_numericality_of :posts_counter, greater_than_or_equal_to: 0, only_integer: true
+
   has_many :posts, inverse_of: :author, dependent: :destroy
   has_many :likes, inverse_of: :author, dependent: :destroy
   has_many :comments, inverse_of: :author, dependent: :destroy
