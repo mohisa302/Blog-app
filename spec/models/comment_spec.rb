227042a6b70768e33800ce37e1828285e9fc06a1
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-06 19:58:01 +0330

    Finish like and comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) { User.create(name: 'Jane', photo: 'https://t3.ftcdn.net/jpg/02/47/40/98/360_F_247409832_pPugfgU5cKLsrH5OCJRMn5JTcy2L1Rrg.jpg', bio: 'Anything', posts_counter: 0) }
+  let(:post) { Post.create(title: 'Test Post', text: 'Lorem ipsum', comments_counter: 0, likes_counter: 0, author_id: user.id) }
 
+  describe "callbacks" do
+    it "increments the post's comment counter after a new comment is created" do
+      expect {
+        Comment.create(author_id: user.id, post_id: post.id)
+      }.to change { post.reload.comments_counter }.from(0).to(1)
+    end
+  end
 end
