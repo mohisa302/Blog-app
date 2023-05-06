6c12d4edb8338412a547756a2c4ae55e39c1e41a
mohi <mohisadeghi302@gmail.com>
2023-05-06 11:24:01 +0430

    Finish user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, inverse_of: :author, dependent: :destroy
+  has_many :likes, inverse_of: :author, dependent: :destroy
+  has_many :comments, inverse_of: :author, dependent: :destroy
+  
+  def recent_posts()
+    posts.order(created_at: :desc).limit(3)
+  end
 end
