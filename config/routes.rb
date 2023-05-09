33d20cac5341f08dff9e2f740c12bf3831da34a7
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-09 19:18:58 +0330

    Create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./app/**/*.html.erb', './app/**/*.html.slim', './app/**/*.rb', './app/**/*.js'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
