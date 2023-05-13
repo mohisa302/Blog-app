be3431b4c40fef26b81c840af428ae511a414cff
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-13 11:38:14 +0330

    Finish users test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -23,28 +23,18 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "GET #show" do
-    let(:user) { User.create(name: "John Doe") }
+    let(:user) { User.create(name: "John Doe", posts_counter: 0) }
+    let(:post) { user.posts.create(title: 'Post 1', text: 'Text 1', comments_counter: 10, author_id: 1, likes_counter: 1) }
 
     it "returns a successful response" do
-      user.save
-      get user_path(user)
+      get user_post_path(user_id: user.id, id: post.id)
       expect(response).to be_successful
     end
+
+    it "assigns the correct user and post" do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(assigns(:user)).to eq(user)
+      expect(assigns(:post)).to eq(post)
+    end
   end
-  # describe "GET #show" do
-  #   let(:user) { User.create(name: "John Doe") }
-  #   let(:post) { user.posts.create(title: "Post title", body: "Post body") }
-
-  #   it "returns a successful response" do
-  #     get :show, params: { id: user.id }
-  #     expect(response).to be_successful
-  #   end
-    
-  #   it "assigns the correct user and posts" do
-  #     get :show, params: { id: user.id }
-  #     expect(assigns(:user)).to eq(user)
-  #     expect(assigns(:posts)).to eq([post])
-  #   end
-  # end
-
 end
