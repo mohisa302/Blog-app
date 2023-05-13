1ebbfb7043e40fd4843304a676ddc84e53394ece
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-13 10:42:10 +0330

    Create test files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts" do
     it "works! (now write some real specs)" do
-      get users_index_path
+      get posts_index_path
       expect(response).to have_http_status(200)
     end
   end
