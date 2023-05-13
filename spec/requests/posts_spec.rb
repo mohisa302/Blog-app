5841b882689dc3f3aaf05308c8d87d3c6bcbc163
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-13 11:27:24 +0330

    Remove duplicate column

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
+  describe "GET #index" do
+    it "returns a successful response" do
+      get posts_path
+      expect(response).to be_successful
+    end
+
+    it "renders successfully" do
+      get posts_path
+      expect(response).to render_template(:index)
+    end
+
+    it "has a correct status" do
+      get posts_path
+      expect(response.status).to eq(200)
+    end
+    it "renders correct view" do
+      get posts_path
+      expect(response.body).to include('Here is the list of all posts for user A')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "renders the index template" do
-      get '/users'
-      expect(response).to have_http_status(:success)
+  describe "GET #index" do
+    it "returns a successful response" do
+      get users_path
+      expect(response).to be_successful
+    end
+
+    it "renders successfully" do
+      get users_path
       expect(response).to render_template(:index)
+    end
+
+    it "has a correct status" do
+      get users_path
+      expect(response.status).to eq(200)
+    end
+    it "renders correct view" do
+      get users_path
       expect(response.body).to include('Here is a list of all users')
     end
   end
+
+  describe "GET #show" do
+    let(:user) { User.create(name: "John Doe") }
+
+    it "returns a successful response" do
+      user.save
+      get user_path(user)
+      expect(response).to be_successful
+    end
+  end
+  # describe "GET #show" do
+  #   let(:user) { User.create(name: "John Doe") }
+  #   let(:post) { user.posts.create(title: "Post title", body: "Post body") }
+
+  #   it "returns a successful response" do
+  #     get :show, params: { id: user.id }
+  #     expect(response).to be_successful
+  #   end
+    
+  #   it "assigns the correct user and posts" do
+  #     get :show, params: { id: user.id }
+  #     expect(assigns(:user)).to eq(user)
+  #     expect(assigns(:posts)).to eq([post])
+  #   end
+  # end
+
 end
