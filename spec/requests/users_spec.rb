b0e878c1f860d0e88fb6b6fa6d532336e3f33547
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-13 12:10:12 +0330

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,46 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET #index" do
-    it "returns a successful response" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
       get users_path
       expect(response).to be_successful
     end
 
-    it "renders successfully" do
+    it 'renders successfully' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it "has a correct status" do
+    it 'has a correct status' do
       get users_path
       expect(response.status).to eq(200)
     end
-    it "renders correct view" do
+    it 'renders correct view' do
       get users_path
       expect(response.body).to include('Here is a list of all users')
     end
   end
 
-  describe "GET #show" do
-    let(:user) { User.create(name: "John Doe", posts_counter: 0) }
-  
-    it "returns a successful response" do
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'John Doe', posts_counter: 0) }
+
+    it 'returns a successful response' do
       get user_path(user)
       expect(response).to be_successful
     end
-  
-    it "renders successfully" do
+
+    it 'renders successfully' do
       get user_path(user)
       expect(response).to render_template(:show)
     end
-  
-    it "has a correct status" do
+
+    it 'has a correct status' do
       get user_path(user)
       expect(response.status).to eq(200)
     end
 
-    it "renders correct view" do
+    it 'renders correct view' do
       get user_path(user)
       expect(response.body).to include('Here are the detials about User')
     end
