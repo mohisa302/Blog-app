ce3a43c3be9a814ff2e8aae859c0e7d1254a37f0
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-24 12:28:18 +0330

    Finish user show|post index

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -1,33 +1,68 @@
-# require 'application_system_test_case'
+require 'rails_helper'
 
-# class UserPostsTest < ApplicationSystemTestCase
-#   test 'viewing user post index page' do
-#     user = users(:user1)
-#     post1 = posts(:post1_user1)
-#     post2 = posts(:post2_user1)
+RSpec.describe Post, type: :model do
+  author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
+  subject do
+    Post.new(title: 'Physics', text: 'This is not my first post', comments_counter: 10, likes_counter: 10,
+             author_id: author)
+  end
 
-#     visit user_posts_path(user)
+  before { subject.save }
 
-#     # Test for user's profile picture, username, bio.
-#     assert_selector '.user-avatar'
-#     assert_text user.username
-#     assert_text "#{user.posts.count} posts"
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 
-#     # Test for post's title, body, comments, likes and pagination.
-#     assert_text post1.title
-#     assert_text post1.body
-#     assert_text "2 comments"
-#     assert_text "5 likes"
+  it 'title should not be too short' do
+    subject.title = 'a'
+    expect(subject).to_not be_valid
+  end
 
-#     assert_text post2.title
-#     assert_text post2.body
-#     assert_text "0 comments"
-#     assert_text "3 likes"
+  it 'title should not be too long' do
+    subject.title = 'a' * 300
+    expect(subject).to_not be_valid
+  end
 
-#     assert_selector ".pagination", visible: true
+  it 'title should not be empty' do
+    subject.title = ' '
+    expect(subject).to_not be_valid
+  end
 
-#     # Test for redirect to post show page when clicking on a post.
-#     click_on post1.title
-#     assert_current_path post_path(post1)
-#   end
-# end
+  it 'title should have valid value' do
+    expect(subject.title).to eql 'Physics'
+  end
+
+  it 'text should be present' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'text should have valid value' do
+    expect(subject.text).to eql 'This is not my first post'
+  end
+
+  it 'comments_counter should have valid value' do
+    subject.comments_counter = 12.2
+    expect(subject).to_not be_valid
+  end
+
+  it 'comments_counter must not be less than 1' do
+    subject.comments_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes_counter should have valid value' do
+    subject.likes_counter = 12.2
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes_counter must not be less than 1' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'should return 5 recent comments' do
+    expect(subject.recent_comments).to eq(subject.comments.order(created_at: :asc).limit(5))
+  end
+end
--- spec/system/user_show_spec.rb
+++ spec/system/user_show_spec.rb
@@ -1,36 +1,65 @@
-require 'rails_helper'
-
-RSpec.describe 'User profile page', type: :feature do
-  let!(:user) { User.create(username: 'johndoe', email: 'johndoe@example.com', password: 'password') }
-
-  before do
-    3.times { FactoryBot.create(:post, user: user) }
-  end
-
-  it 'displays user information and posts' do
-    visit user_path(user)
-
-    # Check that user information is displayed correctly
-    expect(page).to have_css('img.user-avatar')
-    expect(page).to have_content user.username
-    expect(page).to have_content "Posts: #{user.posts.count}"
-    expect(page).to have_content user.bio
-
-    # Check that the first 3 posts are displayed and there's a button to view all posts
-    within('.user-posts') do
-      expect(page).to have_css('.post', count: 3)
-      expect(page).to have_link('See all posts', href: user_posts_path(user))
+RSpec.describe "User profile page", type: :feature do
+  before(:each) do
+    @user = User.create(
+      name: 'John Doe',
+      bio: 'Web Developer',
+      photo: 'https://via.placeholder.com/150',
+      posts_counter: 0
+    )
+  
+    # Create first 3 posts for this user
+    3.times do |n|
+      @post = @user.posts.create(title: "Title #{n+1}", text: "Lorem ipsum dolor sit amet.",  comments_counter: 0, author_id: 1, likes_counter: 0)
     end
-
-    # Click on a post title and verify that it redirects to the post show page
-    post = user.posts.first
-    within(".post[data-post-id='#{post.id}']") do
-      click_link(post.title)
-    end
-    expect(current_path).to eq(post_path(post))
-
-    # Click the 'View all posts' button and verify that it redirects to the user's posts index page
-    click_link('See all posts')
-    expect(current_path).to eq(user_posts_path(user))
   end
-end
+  
+  it "shows user profile picture" do
+    visit user_path(@user)
+    expect(page).to have_css("img[src*='https://via.placeholder.com/150']")
+  end
+  
+  it "shows user's username" do
+    visit user_path(@user)
+    expect(page).to have_content('John Doe')
+  end
+  
+  it "shows the number of posts the user has written" do
+    visit user_path(@user)
+    expect(page).to have_content('Number of posts: 3')
+  end
+  
+  it "shows the user's bio" do
+    visit user_path(@user)
+    expect(page).to have_content('Web Developer')
+  end
+  
+  it "shows the user's first 3 posts" do
+    visit user_path(@user)
+  
+    # Check that the titles of the first 3 posts are present on the page
+    expect(page).to have_content('Title 1')
+    expect(page).to have_content('Title 2')
+    expect(page).to have_content('Title 3')
+  end
+  
+  it 'displays a button for all user posts' do
+    visit user_path(@user)
+    expect(page).to have_content('See all posts')
+  end
+    
+  it 'click a user post, it redirects to that specific post show page' do
+     # navigate to the user profile page
+    visit user_path(@user)
+    # click the "See all posts" button
+    click_link 'See all posts'
+    # assert that the current URL is the expected one
+    expect(page).to have_current_path(user_posts_path(@user))
+    expect(page).to have_content('Lorem ipsum dolor sit amet.')
+  end
+  
+  it 'click to see all posts, it redirects to the user post index page' do
+    visit user_path(@user)
+    click_link 'See all posts'
+    expect(page).to have_current_path(user_posts_path(@user))
+  end
+  end
