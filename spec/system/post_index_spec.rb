21871fa40f04cd3af5ebfafd5575d619573acfcc
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-25 01:18:37 +0330

    Add post indst index test

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -1,68 +1,96 @@
-require 'rails_helper'
+RSpec.describe 'Post index page', type: :feature do
+  before(:each) do
+    @user = User.create(
+      name: 'Mohi',
+      bio: 'Aspiring FullStack Dev',
+      photo: 'https://via.placeholder.com/150',
+      posts_counter: 2
+    )
 
-RSpec.describe Post, type: :model do
-  author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-  subject do
-    Post.new(title: 'Physics', text: 'This is not my first post', comments_counter: 10, likes_counter: 10,
-             author_id: author)
+    @user2 = User.create(
+      name: 'nati',
+      bio: 'FullStack Dev',
+      photo: 'https://via.placeholder.com/150',
+      posts_counter: 2
+    )
+
+    @post1 = Post.create(
+      title: 'Test',
+      text: 'First Post',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
+    )
+
+    @post2 = Post.create(
+      title: 'Test 2',
+      text: 'Second Post',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user2
+    )
+
+    @comment1 = Comment.create(
+      text: 'First Comment',
+      post: @post1,
+      author: @user2
+    )
+
+    @comment2 = Comment.create(
+      text: 'Second Comment',
+      post: @post1,
+      author: @user2
+    )
+
+    @like1 = Like.create(
+      post: @post1,
+      author: @user2
+    )
   end
 
-  before { subject.save }
+  describe 'the post index page' do
+    it 'displays user profile picture' do
+      visit user_posts_path(@user)
+      expect(page).to have_css("img[src*='https://via.placeholder.com/150']")
 
-  it 'title should be present' do
-    subject.title = nil
-    expect(subject).to_not be_valid
-  end
+    end
 
-  it 'title should not be too short' do
-    subject.title = 'a'
-    expect(subject).to_not be_valid
-  end
+    it 'displays the user username' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('Mohi')
+    end
 
-  it 'title should not be too long' do
-    subject.title = 'a' * 300
-    expect(subject).to_not be_valid
-  end
+    it 'displays number of posts the user has written' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('Number of posts: 3')
+    end
 
-  it 'title should not be empty' do
-    subject.title = ' '
-    expect(subject).to_not be_valid
-  end
+    it 'displays how many likes a post has' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('likes: 1')
+    end
 
-  it 'title should have valid value' do
-    expect(subject.title).to eql 'Physics'
-  end
+    it 'displays some of the posts body' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('First Post')
+    end
 
-  it 'text should be present' do
-    subject.text = nil
-    expect(subject).to_not be_valid
-  end
+    it 'displays posts title.' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('Test')
+    end
 
-  it 'text should have valid value' do
-    expect(subject.text).to eql 'This is not my first post'
-  end
+    it 'displays the first comments on a post.' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('First Comment')
+    end
 
-  it 'comments_counter should have valid value' do
-    subject.comments_counter = 12.2
-    expect(subject).to_not be_valid
-  end
+    it 'section for pagination if there are more posts than fit on the view' do
+      visit user_posts_path(@user)
+  
+    end
 
-  it 'comments_counter must not be less than 1' do
-    subject.comments_counter = -1
-    expect(subject).to_not be_valid
-  end
-
-  it 'likes_counter should have valid value' do
-    subject.likes_counter = 12.2
-    expect(subject).to_not be_valid
-  end
-
-  it 'likes_counter must not be less than 1' do
-    subject.likes_counter = -1
-    expect(subject).to_not be_valid
-  end
-
-  it 'should return 5 recent comments' do
-    expect(subject.recent_comments).to eq(subject.comments.order(created_at: :asc).limit(5))
+    it 'click on a post, it redirects me to that post show page.' do
+    end
   end
 end
