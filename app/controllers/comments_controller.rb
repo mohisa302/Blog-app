47c8a3cd46672c10b870b83a2a15a3fe13bb5fb3
Mohi <MOHISADEGHI302@GMAIL.com>
2023-05-25 13:16:19 +0330

    Add user authentication to like|comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,23 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.new
     @post = Post.find(params[:post_id])
+    puts "@post: #{@post.inspect}"
+    @comment = @post.comments.build
   end
-
   def create
-    @comment = current_user.comments.new(comment_params)
     @post = Post.find(params[:post_id])
-    @comment.post_id = @post.id
+    @comment = @post.comments.build(comment_params)
+    @comment.author_id = current_user.id
+  
     if @comment.save
-      flash[:success] = 'Comment created successfully'
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
     else
-      flash[:danger] = "Couldn't create comment"
+      render :new
     end
-    redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
   end
-
+  
   private
-
+  
   def comment_params
     params.require(:comment).permit(:text)
   end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,16 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    @post = Post.find(params[:post_id])
-    @user = current_user
-    @like = Like.new(author: @user, post: @post)
-    @like.save
-    flash[:success] = 'Liked successfully!'
-    redirect_to user_post_path(@user, @post)
+    like_params = params.require(:like).permit(:post_id)
+    @like = current_user.likes.build(like_params.merge(post_id: like_params[:post_id]))
+
+    if @like.save
+      flash[:notice] = 'Like created successfully'
+    else
+      flash[:alert] = 'Like creation failed'
+    end
+
+    redirect_to user_post_path(@like.post.author_id, @like.post.id)
   end
 end
